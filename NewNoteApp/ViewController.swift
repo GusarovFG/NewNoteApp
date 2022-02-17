c78fb3b32bca64c0684e4fce42775e03ec4c8579
Фаддей Гусаров <gusarovfg@gmail.com>
2022-02-17 17:07:19 +0400

    Initial Commit

---
+++ NewNoteApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewNoteApp
+//
+//  Created by Фаддей Гусаров on 17.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
